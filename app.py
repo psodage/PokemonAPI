ee2e36686265f4f5683f490eb0bdcfa72cb0aae2
Prathamesh Sodage <psodage@gmail.com>
2025-05-08 18:47:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask, request, jsonify, send_from_directory
+
+app = Flask(__name__, static_folder='.')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/pokemon', methods=['POST'])
+def get_pokemon_details():
+    data = request.get_json(force=True)
+    
+    try:
+        pokemon_number = int(data.get('pokemon_number', 0))
+    except ValueError:
+        return jsonify({"error": "Invalid input. Pokémon number must be an integer."}), 400
+    
+    if not (1 <= pokemon_number <= 1025):
+        return jsonify({"error": "Invalid Pokémon number. Please provide a number between 1 and 1025."}), 400
+    
+    pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_number}"
+    response = requests.get(pokemon_url)
+    
+    if response.status_code != 200:
+        return jsonify({"error": "Failed to fetch Pokémon details from the API."}), 500
+    
+    pokemon_data = response.json()
+    
+    return jsonify({
+        "pokemon_number": pokemon_number,
+        "name": pokemon_data['name'].capitalize(),
+        "image_data": pokemon_data['sprites']['front_default']
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
